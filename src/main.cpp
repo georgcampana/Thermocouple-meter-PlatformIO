c51e638ba75c7b1b7e77b28aa52e18e322135ca1
Georg Campana <georg.campana@gmail.com>
2020-04-14 00:39:16 +0200

    First fully working version

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <Encoder.h>
 #include <LiquidCrystal_I2C.h>
 #include <max6675.h>
-
+#include <EEPROM.h>
 
 const int encoder_skpin = DD2;
 const int encoder_sk1pin = DD3;
@@ -20,21 +20,29 @@ const int thermosensor_spics = SS;
 // make a cute degree symbol
 uint8_t degree[8]  = {140,146,146,140,128,128,128,128};
 
-
 Encoder inputRotary(encoder_skpin, encoder_sk1pin);
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 MAX6675 thermocouple;
 
-
 int modepage = 0; //mainloop
-uint32_t lastchange_time = 0;
-uint32_t looptime;
+uint32_t lastinputchange_time = 0;
+uint32_t looptime; // kept global to allow other subloops to access the current loop time
 double currenttemp;
 double maxtemp;
 double mintemp;
 double offsettemp=0;
 
 void setup() {
+
+  int offset_int;
+  EEPROM.get(0,offset_int); // let's read from address 0
+  if(offset_int > 50 || offset_int < -50) {
+    // let's init properly
+    EEPROM.write(0,((int)(offsettemp*4))) ;
+  }
+  else {
+    offsettemp = offset_int / 4;
+  }
 
   pinMode(encoder_key, INPUT);
   
@@ -50,150 +58,254 @@ void setup() {
 
   thermocouple.begin(thermosensor_spisck,thermosensor_spics,thermosensor_spimosi);
 
-  lastchange_time = millis();
+  lastinputchange_time = millis();
+
+  mintemp = 2000;
+  maxtemp = -2000; // dummy values out of the sensor range
 
 }
 
-int executedsec;
-void mainloop() {
+uint32_t elapsed_secs;
+uint32_t executed_sec;
 
-  // we do the stuff only once per sec 
-  int elapsed_secs = (looptime - lastchange_time) / 1024;
-  if(elapsed_secs == executedsec) return;
-  executedsec = elapsed_secs;
+bool mainloop(bool button) {
 
   // temp reading loop
-  if(elapsed_secs & 1) { // we read at odd 
-    currenttemp = thermocouple.readCelsius();
-    if(currenttemp > maxtemp) maxtemp = currenttemp;
-    if(currenttemp < mintemp) mintemp = currenttemp;
-  }
-  else { // and display at even
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("< Current Temp >");
-    // go to line #1
-    lcd.setCursor(1,1);
-    lcd.print('C');
-    lcd.write((byte)0);
-    lcd.print(currenttemp);
-  }
+ 
+  //lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("< Thermocouple >");
+  // go to line #1
+  lcd.setCursor(0,1);
+  lcd.print(" C");
+  lcd.write((byte)0);
+  lcd.print(currenttemp);
+  lcd.print("        ");
+
+
+  return false;
 }
 
-void viewmaxloop() {
-  // we do the stuff only once per sec 
-  int elapsed_secs = (looptime - lastchange_time) / 2048;
-  if(elapsed_secs == executedsec) return;
-  executedsec = elapsed_secs;
- 
+bool viewmaxloop(bool button) {
+
+  if(button) maxtemp = currenttemp; 
   // show max 
-  lcd.clear();
+  //lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("< Max T (reset)>");
   // go to line #1
-  lcd.setCursor(1,1);
-  lcd.print('C');
+  lcd.setCursor(0,1);
+  lcd.print(" C");
   lcd.write((byte)0);
   lcd.print(maxtemp);
+  lcd.print("        ");
+  return false;
 }
 
-void viewminloop() {
+bool viewminloop(bool button) {
 
-  // we do the stuff only once per 2 secs 
-  int elapsed_secs = (looptime - lastchange_time) / 1024;
-  if(elapsed_secs == executedsec) return;
-  executedsec = elapsed_secs;
- 
+  if(button) mintemp = currenttemp;
+
   // show min 
-  lcd.clear();
+  //lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("< Min T (reset)>");
   // go to line #1
-  lcd.setCursor(1,1);
-  lcd.print('C');
+  lcd.setCursor(0,1);
+  lcd.print(" C");
   lcd.write((byte)0);
   lcd.print(mintemp);
+  lcd.print("        ");
+  return false;
 }
 
-void setOffsetloop() {
-  // we do the stuff only once per 2 secs 
-  int elapsed_secs = (looptime - lastchange_time) / 1024;
-  if(elapsed_secs == executedsec) return;
-  executedsec = elapsed_secs;
- 
-  // show min 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("< Offset T (set)  >");
-  // go to line #1
-  lcd.setCursor(1,1);
-  lcd.print('C');
-  lcd.write((byte)0);
-  lcd.print(offsettemp);
+bool editoffset = false;
+bool setOffsetloop(bool button) {
+
+  if(button) {
+    editoffset = !editoffset;
+    if(editoffset) {
+      inputRotary.write((int32_t)(offsettemp * 16)); // encoder is 4 per tick and max6675 has 4 values per 1 C
+    }
+    else {
+      offsettemp = (double)(inputRotary.read()) / 16;
+      EEPROM.write(0,((int)(offsettemp*4))) ;
+      inputRotary.write(modepage * 4);
+      lcd.clear();
+    }
+  }    
+
+  if(editoffset) {
+    //lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Edit offset(set)");
+    // go to line #1
+    lcd.setCursor(0,1);
+    lcd.print("<-->  C");
+    lcd.write((byte)0);
+    lcd.print((double)(inputRotary.read()) / 16);
+    lcd.print("        ");
+  }
+  else {
+    //lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("<Offset T (set)>");
+    // go to line #1
+    lcd.setCursor(0,1);
+    lcd.print(" C");
+    lcd.write((byte)0);
+    lcd.print(offsettemp);
+    lcd.print("         ");
+  }
+  return editoffset;
 }
 
-void aboutloop() {
-  // we do the stuff only once per 2 secs 
-  int elapsed_secs = (looptime - lastchange_time) / 1024;
-  if(elapsed_secs == executedsec) return;
-  executedsec = elapsed_secs;
- 
-  // show min 
-  lcd.clear();
+bool aboutloop(bool button) {
+  //lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("<    About!    >");
   // go to line #1
   lcd.setCursor(0,1);
-  lcd.print("Vers: 1.0");
+  lcd.print("Vers:1.0 - Georg");
+
+  return false;
 }
 
+bool editreset = false;
+int currentrotaryval;
+bool resetloop(bool button) {
+  if(button) {
+    editreset = !editreset;
 
-uint8_t light = 1;
-void loop() {
+    if(editreset) {
+      currentrotaryval = inputRotary.read();
+    }
+    else {
+      offsettemp = 0;
+      mintemp = 2000;
+      maxtemp = -2000; // dummy values out of the sensor range
 
-  looptime = millis();
+      EEPROM.write(0,offsettemp);
+    }
+  }
 
-  int newvalue = inputRotary.read() >> 2; // divide by 4
-  if(newvalue == modepage) {
-    // anything to do globally due to idle status (e.g. light off)
-    if(looptime - lastchange_time > 5000) {
-      lcd.setBacklight(0);
-      light = 0;
-      lastchange_time = looptime;
+  if(editreset) {
+    //lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("<    Reset!    >");
+    // go to line #1
+    lcd.setCursor(0,1);
+    lcd.print("CONFIRM or move!");
+    if(inputRotary.read() != currentrotaryval) {
+      // moved to escape confirmation
+      inputRotary.write(currentrotaryval);
+      editreset = false;
     }
   }
   else {
-    modepage = newvalue;
-    lastchange_time = looptime;
-    executedsec = 0; // make sure the stuff gets executed when changing loop
-    if(light == 0) {
-      lcd.setBacklight(1);
-      light = 1; 
-    }  
+    //lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("<    Reset!    >");
+    // go to line #1
+    lcd.setCursor(0,1);
+    lcd.print("-Press to reset-");
   }
 
-  // loops are called only if: 1 sec is elapsed, 
+  return editreset;
+}
+
+uint8_t light = 1;
+bool buttonstatus = false;
+uint32_t lastbutton_ts;
+
+bool modepage_locked = false;
+int lastrotary_val = 0;
+
+void loop() {
+
+  bool encoder_changed = false;
+  bool second_changed = false;
+  bool button_changed = false;
+
+  looptime = millis();
+  elapsed_secs = looptime / 1024; // not really precise but quite fast
+
+  if(executed_sec != elapsed_secs) {
+    second_changed = true;
+  }
+
+  if((looptime - lastbutton_ts) > 200) { // debounce
+
+    bool buttonvalue = (digitalRead(encoder_key) == LOW) ? true:false; // NOTE: inverted: my button is "normally high"
+    if(buttonvalue != buttonstatus) {
+      lastbutton_ts = looptime;
+      buttonstatus = buttonvalue;
+      button_changed = true;
+      lastinputchange_time = looptime;
+    }
+  }
   
+  int newvalue = inputRotary.read() >> 2; // divide by 4
+  if(newvalue != lastrotary_val) {
+    lastrotary_val = newvalue;
+    encoder_changed = true;
+    lastinputchange_time = looptime;
+    if(!modepage_locked){
+      if(newvalue != modepage) {
+        modepage = newvalue;
+      }
+    }
+  }
+  else {
+    // anything to do globally due to idle status (e.g. light off)
+    if(looptime - lastinputchange_time > 5000) {
+      lcd.setBacklight(0);
+      light = 0;
+      lastinputchange_time = looptime;
+    }
+  }
+
+  // loops are called only if: 1 sec is elapsed, the modepage changed or the button has been pushed / released
+  if(!second_changed && !button_changed && !encoder_changed  ) {
+    return; // nothing to do
+  }
+  
+  if((button_changed || encoder_changed) && light == 0) {
+    lcd.setBacklight(1);
+    light = 1; 
+  }
+
+  {
+    currenttemp = thermocouple.readCelsius();
+    currenttemp += offsettemp;
+    if(currenttemp > maxtemp) maxtemp = currenttemp;
+    if(currenttemp < mintemp) mintemp = currenttemp;
+  }
+  executed_sec = elapsed_secs;
+
   switch(modepage) {
 
     case 0:
-      mainloop();
+      modepage_locked = mainloop(buttonstatus);
       break;
     case 1:
-      viewmaxloop();
+      modepage_locked = viewmaxloop(buttonstatus);
       break;
     case 2:
-      viewminloop();
+      modepage_locked = viewminloop(buttonstatus);
       break;
     case 3:
-      setOffsetloop();
+      modepage_locked = setOffsetloop(buttonstatus);
       break;
     case 4:
-      aboutloop();
+      modepage_locked = aboutloop(buttonstatus);
+      break;
+    case 5:
+      modepage_locked = resetloop(buttonstatus);
       break;
     default:
-      if(modepage > 4) modepage = 0;
-      if(modepage < 0) modepage = 4;
+      if(modepage > 5) modepage = 0;
+      if(modepage < 0) modepage = 5;
       inputRotary.write(modepage<<2); // seems that the value is allways a multiple of 4 so we multiply the pageno by 4
   }
 
